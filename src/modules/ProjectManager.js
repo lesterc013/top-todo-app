774493c9a4686e38cef76000b53b06a80289f4d4
mtse-big-best <stetcapdev@gmail.com>
2026-06-12 16:30:30 +0800

    refactor(ProjectManager): Implement CollectionManager interface

--- src/modules/ProjectManager.js
+++ src/modules/ProjectManager.js
@@ -1,4 +1,5 @@
 import Todo from './Todo.js';
+import CollectionManager from './CollectionManager.js';
 import protectedTodoProperties from './protectedTodoProperties.js';
 
 /**
@@ -10,52 +11,60 @@ export default class ProjectManager {
     this.id = id;
     this.projectName = projectName;
     this.todos = new Map();
+
+    // Provide CollectionManager with the factory function that it can use to create Todos.
+    this.todoCollection = new CollectionManager(
+      (id, todoValues) => new Todo(id, todoValues),
+    );
   }
 
-  // Create - instantiate a new Todo object, and store it in todos array
-  // Returns the new todo to caller.
-  // TODO: Should provide an object containing all the properties of the todo rather than just a title - cos this at the end of the day will originate from a form.
-  addTodo(title) {
-    const newTodo = new Todo(crypto.randomUUID(), title);
-    this.todos.set(newTodo.id, newTodo);
-    return newTodo;
+  addTodo(userInput) {
+    // First few properties are the default values. Then spread what the user wrote to override the values.
+    // Then, use the todoCollection to create the todo.
+    const todoValues = {
+      title: '',
+      description: '',
+      dueDate: null,
+      isDone: false,
+      ...userInput,
+    };
+    return this.todoCollection.createOne(todoValues);
   }
 
   // Return all todos to the caller
   getAllTodos() {
-    return this.todos;
+    return this.todoCollection;
   }
 
   // Update
-  updateTodo(id, submittedValues) {
-    const toUpdate = this.todos.get(id);
+  updateTodo(id, userInput) {
+    const toUpdate = this.todoCollection.getOne(id);
     if (!toUpdate) {
       console.log(`Error: ${id} not found.`);
       return null;
     }
 
-    for (const [propertyToUpdate, updatedValue] of Object.entries(
-      submittedValues,
-    )) {
+    // Skip updating if a property is protected.
+    for (const [propertyToUpdate, updatedValue] of Object.entries(userInput)) {
       if (protectedTodoProperties.has(propertyToUpdate)) {
         console.log(`Skipping property: ${propertyToUpdate}`);
         continue;
       }
 
+      // Skip update if property not in Todo schema.
       if (!Object.hasOwn(toUpdate, propertyToUpdate)) {
         console.log(`Error: property: ${propertyToUpdate} not found in todo.`);
         continue;
       }
 
-      // Only update if not 'id', and property exists.
+      // Only update after the checks.
       toUpdate[propertyToUpdate] = updatedValue;
     }
 
     return toUpdate;
   }
 
-  // Delete. Return T/F if todo has been successfully deleted.
   deleteTodo(id) {
-    return this.todos.delete(id);
+    return this.todoCollection.deleteOne(id);
   }
 }
