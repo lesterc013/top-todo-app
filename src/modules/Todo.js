2d7f9d414761eb865823b7c5e360fc097381fcdd
mtse-big-best <stetcapdev@gmail.com>
2026-06-12 16:28:21 +0800

    refactor(Todo): Take in values object in Todo instead of only title
    This correctly replicates what we would expect from the form a user will submit.

--- src/modules/Todo.js
+++ src/modules/Todo.js
@@ -2,13 +2,11 @@
  * Keep the Todo class simple - only a data storage class.
  */
 export default class Todo {
-  constructor(id, title) {
+  constructor(id, values) {
     this.id = id;
-    this.title = title;
-    this.description = 'Default';
-    this.dueDate = 'To set a date object';
-    this.priority =
-      'Some dynamically set number based on some indexing logic of a todo';
-    this.isDone = false;
+    this.title = values.title;
+    this.description = values.description;
+    this.dueDate = values.dueDate;
+    this.isDone = values.isDone;
   }
 }
