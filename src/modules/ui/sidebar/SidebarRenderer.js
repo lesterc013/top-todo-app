61f60052ca3f06b3e7a3d5f0816ce30e011e3fe1
mtse-big-best <stetcapdev@gmail.com>
2026-06-15 14:26:44 +0800

    feat: Implement SidebarRenderer
    Is given the container to render the html elements in and the ProjectManager object.
    
    Creates the ul in the container.

---
+++ src/modules/ui/sidebar/SidebarRenderer.js
@@ -0,0 +1,32 @@
+/**
+ * Renders the Sidebar through data provided by ProjectManager
+ */
+export default class SidebarRenderer {
+  constructor(renderContainer, projectManager) {
+    this.renderContainer = renderContainer;
+    this.projectManager = projectManager;
+  }
+
+  // Create a list of the project names. Render it in the sidebar as a ul first.
+  render() {
+    this.renderContainer.replaceChildren();
+
+    const ul = document.createElement('ul');
+
+    // Map returned by the ProjectManager method.
+    const todoManagers = this.projectManager.getAllTodoManagers();
+    for (const todoManager of todoManagers.values()) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = `/${todoManager.id}`;
+      const span = document.createElement('span');
+      span.textContent = `${todoManager.projectName}`;
+
+      a.appendChild(span);
+      li.appendChild(a);
+      ul.appendChild(li);
+    }
+
+    this.renderContainer.appendChild(ul);
+  }
+}
