baab1b5016535925bd087148328d9b8727f42668
mtse-big-best <stetcapdev@gmail.com>
2026-06-17 17:22:24 +0800

    feat(TodoController): Implement add new todo to activeTodoManager
    Also removed keeping track of the current todo manager cos I can just inject it. Cleaner that way.

--- src/modules/controller/TodoController.js
+++ src/modules/controller/TodoController.js
@@ -1,22 +1,29 @@
 export default class TodoController {
-  constructor(projectManager, mainRenderer) {
-    this.projectManager = projectManager;
+  constructor(mainRenderer) {
     this.mainRenderer = mainRenderer;
   }
 
-  handleUpdateTodo(formData) {
+  #preprocessForm(formData) {
     const formDataObj = Object.fromEntries(formData.entries());
-
-    // Need to manually create the isDone property because when checkboxes are ticked, they will appear in the submission and vice versa.
     formData.has('isDone')
       ? (formDataObj.isDone = true)
       : (formDataObj.isDone = false);
-    console.log(formDataObj);
+    return formDataObj;
+  }
 
-    // Call the activeTodoManager to update the todo
-    const active = this.projectManager.activeTodoManager;
-    active.updateTodo(formDataObj.id, formDataObj);
+  handleUpdateTodo(activeTodoManager, formData) {
+    const formDataObj = this.#preprocessForm(formData);
+
+    activeTodoManager.updateTodo(formDataObj.id, formDataObj);
     // Then rerender the todos
-    this.mainRenderer.renderMainContainerFor(active);
+    this.mainRenderer.renderMainContainerFor(activeTodoManager);
+  }
+
+  handleAddTodo(activeTodoManager, formData) {
+    const formDataObj = this.#preprocessForm(formData);
+
+    activeTodoManager.addTodo(formDataObj);
+    // Then rerender the todos
+    this.mainRenderer.renderMainContainerFor(activeTodoManager);
   }
 }
